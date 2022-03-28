21abedd64604b2f20cb9f34c5bdb3b61283d142e
chainedfarmsnetwork <chainedfarmsnetwork@gmail.com>
2022-03-28 20:54:03 +0200

    update app.py

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python3
-import os
-
 import aws_cdk as cdk
 
-from aws_cdk_jenkins_cicd.aws_cdk_jenkins_cicd_stack import AwsCdkJenkinsCicdStack
+from back.back_stack import BackStack
+from front.front_stack import FrontStack
 
 
 app = cdk.App()
-AwsCdkJenkinsCicdStack(app, "AwsCdkJenkinsCicdStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+ENV = cdk.core.Environment(
+    region=app.node.try_get_context("AWS_REGION"),
+    account=app.node.try_get_context("AWS_ACCOUNT"),
+)
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+SUFFIX = app.node.try_get_context("SUFFIX")
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+BackStack(app, f"back-{SUFFIX}", ENV)
+FrontStack(app, f"back-{SUFFIX}", ENV)
 
 app.synth()
